ef5342bfda0216986151ba59e3b5da63bc83b00b
az71132 <92398278+az71132@users.noreply.github.com>
2025-06-24 13:07:22 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const app = express();
-app.use(express.json());
+app.use(express.json()); 
 
 const SHOPIFY_API_KEY = process.env.SHOPIFY_API_KEY;
 const SHOPIFY_API_PASSWORD = process.env.SHOPIFY_API_PASSWORD;
